79491edd6abb254646ea8bb3410ee520f6ded72a
DariuszRozmus <drozmus@student.agh.edu.pl>
2024-11-29 16:28:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wyświetlanie wiadomości w konsoli
+console.log("Witaj na mojej stronie!");
+
+// Funkcja zmieniająca kolor przycisku po kliknięciu
+function zmienKolor() {
+    document.body.style.backgroundColor = "lightblue";
+}
